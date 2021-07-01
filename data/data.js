0741fd9c73356e3c5dcd90e4301e8f5a36c8a703
Rodrigo Novaes <rod_novaes@hotmail.com>
2021-07-02 11:08:22 +1200

    add database

---
+++ data/data.js
@@ -0,0 +1,322 @@
+const db = [
+ {
+   id: '1',
+   url: '/images/meme1.jpg',
+ },
+ {
+   id: '2',
+   url: '/images/meme2.jpg',
+ },
+ {
+   id: '3',
+   url: '/images/meme3.jpg',
+ },
+ {
+   id: '4',
+   url: '/images/meme4.jpg',
+ },
+ {
+   id: '5',
+   url: '/images/meme5.jpg',
+ },
+ {
+   id: '6',
+   url: '/images/meme6.jpg',
+ },
+ {
+   id: '7',
+   url: '/images/meme7.jpg',
+ },
+ {
+   id: '8',
+   url: '/images/meme8.jpg',
+ },
+ {
+   id: '9',
+   url: '/images/meme9.jpg',
+ },
+ {
+   id: '10',
+   url: '/images/meme10.jpg',
+ },
+ {
+   id: '11',
+   url: '/images/meme11.jpg',
+ },
+ {
+   id: '12',
+   url: '/images/meme12.jpg',
+ },
+ {
+   id: '13',
+   url: '/images/meme13.jpg',
+ },
+ {
+   id: '14',
+   url: '/images/meme14.jpg',
+ },
+ {
+   id: '15',
+   url: '/images/meme15.jpg',
+ },
+ {
+   id: '16',
+   url: '/images/meme16.jpg',
+ },
+ {
+   id: '17',
+   url: '/images/meme17.jpg',
+ },
+ {
+   id: '18',
+   url: '/images/meme18.jpg',
+ },
+ {
+   id: '19',
+   url: '/images/meme19.jpg',
+ },
+ {
+   id: '20',
+   url: '/images/meme20.jpg',
+ },
+ {
+   id: '21',
+   url: '/images/meme21.jpg',
+ },
+ {
+   id: '22',
+   url: '/images/meme22.jpg',
+ },
+ {
+   id: '23',
+   url: '/images/meme23.jpg',
+ },
+ {
+   id: '24',
+   url: '/images/meme24.jpg',
+ },
+ {
+   id: '25',
+   url: '/images/meme25.jpg',
+ },
+ {
+   id: '26',
+   url: '/images/meme26.jpg',
+ },
+ {
+   id: '27',
+   url: '/images/meme27.jpg',
+ },
+ {
+   id: '28',
+   url: '/images/meme28.jpg',
+ },
+ {
+   id: '29',
+   url: '/images/meme29.jpg',
+ },
+ {
+   id: '30',
+   url: '/images/meme30.jpg',
+ },
+ {
+   id: '31',
+   url: '/images/meme31.jpg',
+ },
+ {
+   id: '32',
+   url: '/images/meme32.jpg',
+ },
+ {
+   id: '33',
+   url: '/images/meme33.jpg',
+ },
+ {
+   id: '34',
+   url: '/images/meme34.jpg',
+ },
+ {
+   id: '35',
+   url: '/images/meme35.jpg',
+ },
+ {
+   id: '36',
+   url: '/images/meme36.jpg',
+ },
+ {
+   id: '37',
+   url: '/images/meme37.jpg',
+ },
+ {
+   id: '38',
+   url: '/images/meme38.jpg',
+ },
+ {
+   id: '39',
+   url: '/images/meme39.jpg',
+ },
+ {
+   id: '40',
+   url: '/images/meme40.jpg',
+ },
+ {
+   id: '41',
+   url: '/images/meme41.jpg',
+ },
+ {
+   id: '42',
+   url: '/images/meme42.jpg',
+ },
+ {
+   id: '43',
+   url: '/images/meme43.jpg',
+ },
+ {
+   id: '44',
+   url: '/images/meme44.jpg',
+ },
+ {
+   id: '45',
+   url: '/images/meme45.jpg',
+ },
+ {
+   id: '46',
+   url: '/images/meme46.jpg',
+ },
+ {
+   id: '47',
+   url: '/images/meme47.jpg',
+ },
+ {
+   id: '48',
+   url: '/images/meme48.jpg',
+ },
+ {
+   id: '49',
+   url: '/images/meme49.jpg',
+ },
+ {
+   id: '50',
+   url: '/images/meme50.jpg',
+ },
+ {
+   id: '51',
+   url: '/images/meme51.jpg',
+ },
+ {
+   id: '52',
+   url: '/images/meme52.jpg',
+ },
+ {
+   id: '53',
+   url: '/images/meme53.jpg',
+ },
+ {
+   id: '54',
+   url: '/images/meme54.jpg',
+ },
+ {
+   id: '55',
+   url: '/images/meme55.jpg',
+ },
+ {
+   id: '56',
+   url: '/images/meme56.jpg',
+ },
+ {
+   id: '57',
+   url: '/images/meme57.jpg',
+ },
+ {
+   id: '58',
+   url: '/images/meme58.jpg',
+ },
+ {
+   id: '59',
+   url: '/images/meme59.jpg',
+ },
+ {
+   id: '60',
+   url: '/images/meme60.jpg',
+ },
+ {
+   id: '61',
+   url: '/images/meme61.jpg',
+ },
+ {
+   id: '62',
+   url: '/images/meme62.jpg',
+ },
+ {
+   id: '63',
+   url: '/images/meme63.jpg',
+ },
+ {
+   id: '64',
+   url: '/images/meme64.jpg',
+ },
+ {
+   id: '65',
+   url: '/images/meme65.jpg',
+ },
+ {
+   id: '66',
+   url: '/images/meme66.jpg',
+ },
+ {
+   id: '67',
+   url: '/images/meme67.jpg',
+ }, 
+ {
+   id: '68',
+   url: '/images/meme68.jpg',
+ },
+ {
+   id: '69',
+   url: '/images/meme69.jpg',
+ },
+ {
+   id: '70',
+   url: '/images/meme70.jpg',
+ },
+ {
+   id: '71',
+   url: '/images/meme71.jpg',
+ },
+ {
+   id: '72',
+   url: '/images/meme72.jpg',
+ },
+ {
+   id: '73',
+   url: '/images/meme73.jpg',
+ },
+ {
+   id: '74',
+   url: '/images/meme74.jpg',
+ },
+ {
+   id: '75',
+   url: '/images/meme75.jpg',
+ },
+ {
+   id: '76',
+   url: '/images/meme76.jpg',
+ },
+ {
+   id: '77',
+   url: '/images/meme77.jpg',
+ },
+ {
+   id: '78',
+   url: '/images/meme78.jpg',
+ },
+ {
+   id: '79',
+   url: '/images/meme79.jpg',
+ },
+ {
+   id: '80',
+   url: '/images/meme80.jpg',
+ },
+]
